2680bad5f4ede676c3e4d49c216c43cfd7fe0b8e
Jay Madu <jmaduafokwa@hotmail.com>
2024-08-22 11:25:58 +0100

    added logic to either optimize or unoptimize images

--- src/components/WorkDetail/MainPage.tsx
+++ src/components/WorkDetail/MainPage.tsx
@@ -8,6 +8,9 @@ import Link from "next/link";
 import Paragraph from "../Paragraph";
 
 function MainPage({ project }: { project: Projects }) {
+
+    console.log(project.imagesMiddle && project.imagesMiddle[0].src.split('/').pop()?.split('.').pop())
+
   return (
     <main>
       {/* CAROUSEL WITH TOP IMAGE */}
@@ -25,7 +28,7 @@ function MainPage({ project }: { project: Projects }) {
             ].map((proj, i) => {
               return (
                 <h1
-                  key={`${proj}_i`}
+                  key={`${proj}_${i}`}
                   className={`${spectralBridgeRegular.className} leading-[1] mb-[-.15em] text-[23vw] uppercase whitespace-nowrap`}
                 >
                   {proj}
@@ -45,8 +48,8 @@ function MainPage({ project }: { project: Projects }) {
           )}
         </div>
       </section>
+        {/* INFO ABOUT WEBSITE, GITHUB, TOOLS, YEAR, DURATION, ROLES, AND DESC */}
       <section className="my-[9vh] flex flex-col md:flex-row gap-8">
-        {/* WEBSITE, GITHUB, TOOLS, YEAR, DURATION, AND ROLES */}
         <div className="flex-1">
           {/* VISIT WEBSITE AND GITHUB */}
           <div className="flex items-center gap-[8em]">
@@ -87,8 +90,8 @@ function MainPage({ project }: { project: Projects }) {
               })}
             </div>
           </div>
-          {/* YEAR & DURATION */}
           <div className="flex gap-20 mt-6">
+          {/* YEAR & DURATION */}
             <div>
               <div>
                 <Heading text="Year" />
@@ -99,6 +102,7 @@ function MainPage({ project }: { project: Projects }) {
                 {project.duration && <Paragraph text={project?.duration} />}
               </div>
             </div>
+          {/* ROLES */}
             <div>
               <Heading text="Roles" />
               <div className="">
@@ -113,7 +117,7 @@ function MainPage({ project }: { project: Projects }) {
             </div>
           </div>
         </div>
-        {/* PARAGRAPH SECTION */}
+        {/* FIRST PARAGRAPH SECTION */}
         <div className="flex-1 lg:flex-[1.5] flex flex-col gap-y-6">
           {project?.description1?.map((desc) => {
             return (
@@ -129,14 +133,14 @@ function MainPage({ project }: { project: Projects }) {
           <div className="flex flex-col md:flex-row items-center gap-3">
             <div className="flex-1">
               <div className="mb-3">
-                <Image src={project?.imagesMiddle[0]} alt="" unoptimized />
+                <Image src={project?.imagesMiddle[0]} alt="" unoptimized={project.imagesMiddle[0].src.split('/').pop()?.split('.').pop() === 'gif' ?? false} />
               </div>
               <div>
-                <Image src={project?.imagesMiddle[1]} alt="" unoptimized />
+                <Image src={project?.imagesMiddle[1]} alt="" unoptimized={project.imagesMiddle[1].src.split('/').pop()?.split('.').pop() === 'gif' ?? false} />
               </div>
             </div>
             <div className="flex-1">
-              <Image src={project?.imagesMiddle[2]} alt="" unoptimized />
+              <Image src={project?.imagesMiddle[2]} alt="" unoptimized={project.imagesMiddle[2].src.split('/').pop()?.split('.').pop() === 'gif' ?? false} />
             </div>
           </div>
         )}
@@ -163,8 +167,8 @@ function MainPage({ project }: { project: Projects }) {
               <Image
                 src={project?.imagesOutro[0]}
                 alt=""
-                unoptimized
                 className="w-full h-full"
+                unoptimized={project.imagesOutro[0].src.split('/').pop()?.split('.').pop() === 'gif' ?? false}
               />
             </div>
             <div className="flex flex-col md:flex-row gap-3">
@@ -172,16 +176,16 @@ function MainPage({ project }: { project: Projects }) {
                 <Image
                   src={project?.imagesOutro[1]}
                   alt=""
-                  unoptimized
                   className="w-full h-full"
+                  unoptimized={project.imagesOutro[1].src.split('/').pop()?.split('.').pop() === 'gif' ?? false}
                 />
               </div>
               <div className="flex-1 object-cover">
                 <Image
                   src={project?.imagesOutro[2]}
                   alt=""
-                  unoptimized
                   className="w-full h-full"
+                  unoptimized={project.imagesOutro[2].src.split('/').pop()?.split('.').pop() === 'gif' ?? false}
                 />
               </div>
             </div>
