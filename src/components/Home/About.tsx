9dc2bb413ac269fa2f015078d3bdce87f355d7b9
Jay Madu <jmaduafokwa@hotmail.com>
2024-09-11 01:28:10 +0100

    added animations

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import Paragraph from "../Paragraph";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { motion } from "framer-motion";
 
 function About() {
   const image1 = useRef(null);
@@ -15,7 +16,7 @@ function About() {
 
   useGSAP(() => {
     gsap.to(image1.current, {
-      scale: 1.06,
+      y: "10%",
       ease: "power1.inOut",
       scrollTrigger: {
         trigger: image1.current,
@@ -24,7 +25,7 @@ function About() {
         scrub: true,
       },
     });
-    
+
     var tl = gsap.timeline({
       ease: "power1.inOut",
       scrollTrigger: {
@@ -36,42 +37,71 @@ function About() {
     });
 
     tl.from(image2Div.current, {
-      yPercent: 10
+      yPercent: 10,
     }).to(image2Div.current, {
       yPercent: -5,
     });
   });
 
+  const textVariant = {
+    initial: {
+      y: "120%",
+      rotateZ: 5
+    },
+    animate: {
+      y: 0,
+      rotateZ: 0,
+      transition: {
+        duration: 1,
+        ease: [0.87, 0, 0.13, 1],
+        // delay: .2
+      }
+    }
+  }
+
   return (
     <section className="py-[15vh]">
       <div>
-        <h3
-          className={`${spectralBridgeRegular.className} text-[5.5vw] pr-[15vw] leading-[1]`}
-        >
-          I promote <span className="italic">fresh</span> and creative{" "}
-          <span className="italic">ideas</span> and bring them to life.
-        </h3>
-        <h3
-          className={`${spectralBridgeRegular.className} text-[5.5vw] pl-[15vw] leading-[1]`}
-        >
-          With my skills, I <span className="italic">deliver </span>
-          quality products from start to <span className="italic">
-            finish
-          </span>{" "}
-          .
-        </h3>
+        <div className="overflow-hidden">
+          <motion.h3
+            variants={textVariant}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            className={`${spectralBridgeRegular.className} text-[5.5vw] pr-[15vw] leading-[1]`}
+          >
+            I promote <span className="italic">fresh</span> and creative{" "}
+            <span className="italic">ideas</span> and bring them to life.
+          </motion.h3>
+        </div>
+        <div className="overflow-hidden">
+          <motion.h3
+            variants={textVariant}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            className={`${spectralBridgeRegular.className} text-[5.5vw] pl-[15vw] leading-[1]`}
+          >
+            With my skills, I <span className="italic">deliver </span>
+            quality products from start to{" "}
+            <span className="italic">finish</span> .
+          </motion.h3>
+        </div>
       </div>
       <div className="mt-10 flex flex-col-reverse md:flex-row gap-y-4 md:gap-y-0 md:max-h-screen">
         <div className="md:flex-1 object-cover object-center overflow-hidden">
           <Image
             src={Image1}
             alt="sjsh"
-            className="w-full h-full"
+            className="w-full h-full scale-110"
             ref={image1}
           />
         </div>
         <div className="md:flex-1 flex justify-center items-center">
-          <div className="w-full h-full md:w-[80%] md:h-[80%] object-cover object-left-top overflow-hidden" ref={image2Div}>
+          <div
+            className="w-full h-full md:w-[80%] md:h-[80%] object-cover object-left-top overflow-hidden"
+            ref={image2Div}
+          >
             <Image
               src={Image2}
               alt="sjsh"
