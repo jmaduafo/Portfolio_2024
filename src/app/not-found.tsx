4d50f7ce026bd6d47483ef30bb1855843bec3644
Jay Madu <jmaduafokwa@hotmail.com>
2024-09-16 22:55:49 +0100

    adjusted styling in error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -15,17 +15,17 @@ function NotFound() {
     <div className="px-[6vw]">
       <Navbar />
       <main>
-        <section className="">
-          <div className="w-[70%] mx-auto mt-[10vh]">
+        <section className="h-[85vh] flex flex-col justify-center">
+          <div className="w-[70%] mx-auto mt-auto">
             <div className="flex justify-center items-center gap-[1vw]">
               <h1
-                className={`${spectralBridgeRegular.className} text-[23vw] leading-[1] mb-[-.25em]`}
+                className={`${spectralBridgeRegular.className} text-[30vw] md:text-[23vw] leading-[1] mb-[-.25em]`}
               >
                 4
               </h1>
-              <StarSpin classNameSize="w-[15vw]" />
+              <StarSpin classNameSize="w-[20vw] md:w-[15vw]" />
               <h1
-                className={`${spectralBridgeRegular.className} text-[23vw] leading-[1] mb-[-.25em]`}
+                className={`${spectralBridgeRegular.className} text-[30vw] md:text-[23vw] leading-[1] mb-[-.25em]`}
               >
                 4
               </h1>
@@ -35,17 +35,21 @@ function NotFound() {
               <Header3 phrase={line2} className="text-center" />
             </div>
           </div>
-          <div className="flex justify-center xs:justify-end my-[10vh]">
+          <div className="flex justify-center xs:justify-end mt-auto">
             <Link href={"/"}>
               <div className="flex items-center gap-6 hover:gap-8 duration-300 cursor-pointer">
-                <h4 className="text-[3vw] tracking-tighter">GO TO HOME</h4>
-                <Next className="w-[5vw]" />
+                <h4 className="text-[7vw] xs:text-[5vw] sm:text-[3vw] tracking-tighter">
+                  GO TO HOME
+                </h4>
+                <Next className="w-[8vw] xs:w-[6vw] sm:w-[4vw]" />
               </div>
             </Link>
           </div>
         </section>
       </main>
-      <Footer />
+      <div className="mt-16">
+        <Footer />
+      </div>
     </div>
   );
 }
